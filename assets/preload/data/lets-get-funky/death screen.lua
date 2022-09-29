d3c54b3765171beeb78969c5a05ec5b2369186bc
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-09-29 15:07:09 -0300

    Add files via upload

---
+++ assets/preload/data/lets-get-funky/death screen.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'ko-dead');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'kodead');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
+end
